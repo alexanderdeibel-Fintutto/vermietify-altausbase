2772697862c8b117b97fb458bf4c6c39238ec0e5
alexander deibel <alexander.deibel@googlemail.com>
2025-12-28 00:56:17 +0000

    File changes

--- src/components/contracts/RentChangeForm.jsx
+++ src/components/contracts/RentChangeForm.jsx
@@ -13,7 +13,7 @@ import {
     DialogTitle,
 } from "@/components/ui/dialog";
 import { Loader2 } from 'lucide-react';
-import { regenerateContractPayments } from './generatePayments';
+
 
 export default function RentChangeForm({ open, onOpenChange, contract }) {
     const { register, handleSubmit, reset, watch } = useForm();
