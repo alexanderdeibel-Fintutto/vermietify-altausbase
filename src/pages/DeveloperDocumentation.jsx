8b0d59ebcf5b10e224b068c414901f24b9241eaf
alexander deibel <alexander.deibel@googlemail.com>
2026-01-06 23:16:54 +0000

    File changes

--- src/pages/DeveloperDocumentation.jsx
+++ src/pages/DeveloperDocumentation.jsx
@@ -693,11 +693,11 @@ export default function DeveloperDocumentation() {
                         }
                     }}
                     className="text-red-600 hover:text-red-700"
-                >
+                    >
                     <Trash2 className="w-5 h-5 mr-2" />
                     Alle löschen
-                </Button>
-                <Button 
+                    </Button>
+                    <Button 
                     variant="outline" 
                     size="lg"
                     onClick={() => {
@@ -707,11 +707,11 @@ export default function DeveloperDocumentation() {
                             setSelectedTypes(DOCUMENTATION_TYPES.map(t => t.type));
                         }
                     }}
-                >
+                    >
                     <CheckCircle className="w-5 h-5 mr-2" />
                     {selectedTypes.length === DOCUMENTATION_TYPES.length ? 'Keine' : 'Alle'} auswählen
-                </Button>
-                <Button 
+                    </Button>
+                    <Button 
                     variant="outline" 
                     size="lg"
                     onClick={() => setShowScheduleDialog(true)}
