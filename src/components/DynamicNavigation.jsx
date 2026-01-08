692b07536be62f64cf299b713823d22599924b9e
alexander deibel <alexander.deibel@googlemail.com>
2026-01-08 21:46:59 +0000

    File changes

--- src/components/DynamicNavigation.jsx
+++ src/components/DynamicNavigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '@/utils';
 import { usePackageAccess } from '@/components/hooks/usePackageAccess';
 import { ChevronRight } from 'lucide-react';
 import { cn } from "@/lib/utils";
