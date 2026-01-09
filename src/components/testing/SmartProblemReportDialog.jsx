97c1e04e39fdad8538c3fdbc29f0463eb0209d50
alexander deibel <alexander.deibel@googlemail.com>
2026-01-09 04:21:26 +0000

    File changes

--- src/components/testing/SmartProblemReportDialog.jsx
+++ src/components/testing/SmartProblemReportDialog.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import html2canvas from 'html2canvas';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
