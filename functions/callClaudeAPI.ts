b401b1518fce764369e67f1ec791e315d9f461dc
alexander deibel <alexander.deibel@googlemail.com>
2026-01-15 12:01:29 +0000

    File changes

--- functions/callClaudeAPI.ts
+++ functions/callClaudeAPI.ts
@@ -25,7 +25,7 @@ Deno.serve(async (req) => {
                 "content-type": "application/json",
             },
             body: JSON.stringify({
-                model: "claude-3-5-sonnet-20240620",
+                model: "claude-3-5-sonnet-20241022",
                 max_tokens: maxTokens,
                 system: systemPrompt,
                 messages: messages,
