23e1c9f3267bbce99281a508de8dc9f5113ed661
alexander deibel <alexander.deibel@googlemail.com>
2026-01-16 23:01:16 +0000

    File changes

--- src/components/ui/alert.jsx
+++ src/components/ui/alert.jsx
@@ -35,7 +35,7 @@ Alert.displayName = "Alert"
 const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
   <h5
     ref={ref}
-    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    className={cn("font-medium leading-none tracking-tight mb-1", className)}
     {...props} />
 ))
 AlertTitle.displayName = "AlertTitle"
@@ -43,7 +43,7 @@ AlertTitle.displayName = "AlertTitle"
 const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    className={cn("text-sm leading-relaxed", className)}
     {...props} />
 ))
 AlertDescription.displayName = "AlertDescription"
