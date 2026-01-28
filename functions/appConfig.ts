e049bc42ceda5dc0e6f258491bc01bc642cc1e88
alexander deibel <alexander.deibel@googlemail.com>
2026-01-28 00:08:27 +0000

    File changes

--- functions/appConfig.ts
+++ functions/appConfig.ts
@@ -1,3 +1,4 @@
+
 // App-Konfiguration - ZENTRAL DEFINIEREN
 export const APP_CONFIG = {
   id: 'nk-abrechnung',
@@ -5,5 +6,25 @@ export const APP_CONFIG = {
   slug: 'nk-abrechnung',
   categories: ['core', 'tool', 'calculator'],
   targetAudience: ['landlord', 'property_manager', 'caretaker'],
-  description: 'Automatisierte Nebenkostenabrechnungen für Immobilienverwalter'
+  description: 'Automatisierte Nebenkostenabrechnungen für Immobilienverwalter',
+  
+  // Kommunikations-Einstellungen
+  messaging: {
+    appId: 'vermietify',
+    userType: 'landlord',
+    features: {
+      directMessages: true,
+      taskComments: true,
+      documentDiscussions: true,
+      groupChats: true,
+      broadcasts: true
+    }
+  },
+  
+  // MieterApp Integration
+  mieterApp: {
+    appId: '696feb63b8085b338cf6c4e7',
+    baseUrl: 'https://mieterapp.fintutto.de',
+    inviteUrlTemplate: 'https://mieterapp.fintutto.de/invite/{inviteCode}'
+  }
 };
