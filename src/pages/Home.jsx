120645c5c4d62b97c0e68de19fa7df2cc2e55c05
alexander deibel <alexander.deibel@googlemail.com>
2026-02-02 01:03:49 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from '@/utils';
+import { createPageUrl } from '../utils';
 import { ArrowRight, BarChart3, Users, FileText, Bot, Settings } from 'lucide-react';
 
 export default function Home() {
